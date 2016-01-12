301bc2377569681b213a4945e75248b7f020b68e
Marcus Chang <marcus.chang@arm.com>
2016-01-12 17:33:59 +0000

    Added build files.

---
+++ CMake/toolchain.cmake
@@ -0,0 +1,24 @@
+# Copyright (C) 2014 ARM Limited. All rights reserved.
+
+if(TARGET_WATCH_EFM32GG_GCC_TOOLCHAIN_INCLUDED)
+    return()
+endif()
+set(TARGET_WATCH_EFM32GG_GCC_TOOLCHAIN_INCLUDED 1)
+
+# provide compatibility definitions for compiling with this target: these are
+# definitions that legacy code assumes will be defined. Before adding something
+# here, think VERY CAREFULLY if you can't change anywhere that relies on the
+# definition that you're about to add to rely on the TARGET_LIKE_XXX
+# definitions that yotta provides based on the target.json file.
+#
+add_definitions("-DWATCH -DEFM32GG995F1024 -DEFM32GG -DEFM32")
+
+# append non-generic flags, and set EFM32GG-specific link script
+
+set(_CPU_COMPILATION_OPTIONS "-mcpu=cortex-m3 -mthumb -D__thumb2__ -g -gdwarf-3 -g3 -gstrict-dwarf")
+
+set(CMAKE_C_FLAGS_INIT             "${CMAKE_C_FLAGS_INIT} ${_CPU_COMPILATION_OPTIONS}")
+set(CMAKE_ASM_FLAGS_INIT           "${CMAKE_ASM_FLAGS_INIT} ${_CPU_COMPILATION_OPTIONS}")
+set(CMAKE_CXX_FLAGS_INIT           "${CMAKE_CXX_FLAGS_INIT} ${_CPU_COMPILATION_OPTIONS}")
+set(CMAKE_MODULE_LINKER_FLAGS_INIT "${CMAKE_MODULE_LINKER_FLAGS_INIT} -mcpu=cortex-m3 -mthumb")
+set(CMAKE_EXE_LINKER_FLAGS_INIT    "${CMAKE_EXE_LINKER_FLAGS_INIT} -mcpu=cortex-m3 -mthumb -u _printf_float -T\"${CMAKE_CURRENT_LIST_DIR}/../ld/efm32gg.ld\"")
